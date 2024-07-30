4b970cf37cc0e5a706e608cda45a5ba3b15ca270
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-07-30 13:06:09 -0500

    create new console application

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Introduction
+        Console.WriteLine("**************************************");
+        Console.WriteLine("Determine which two integers in an array of integers is closest to zero");
+        Console.WriteLine("**************************************\n");
+    }
+}
